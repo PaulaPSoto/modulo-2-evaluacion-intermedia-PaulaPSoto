4441ad657a113ce10db5f8d1abb5fd9795c4626e
PaulaPSoto <paezsotopaula@gmail.com>
2021-01-28 10:56:54 +0100

    test of game

--- js/main.js
+++ js/main.js
@@ -8,53 +8,27 @@ console.log('El número aleatorio es:' + randomNumber);
 
 const introNumber = document.querySelector('.js-test');
 const game = document.querySelector('.js-numbrergame');
+const writeText = document.querySelector('.js-textgame');
+
 
 function handleButtonClick (event) {
   const selectValue = game.value;
   console.log('El número introducido es: ' + selectValue);
-//   faceInWeb.innerHTML = selectValue;
-//   colorfondo();
-//   cambiacara();
-  }
 
+  if (game===randomNumber) {
 
+    writeText.innerHTML='¡Has Ganado!';
+   } else { 
+    writeText.innerHTML='¡Sigue jugando!'; 
+    
+   }
+}
  introNumber.addEventListener('click', handleButtonClick);
 
 
 
-// const face = document.querySelector ('.js-select');
-// const buttonChange = document.querySelector ('.js-update');
-// const faceInWeb = document.querySelector ('.js-face');
-// const clorbackgroung = document.querySelector ('.js-container');
-
-// function colorfondo (){
-//   const randomNumber = Math.ceil(Math.random() * 100);
-//   const tipo=(randomNumber%2)?"Impar":"Par";
-
-//   if (tipo=== "Par") {
-//     clorbackgroung.classList.add('background-color-alt');
-    
-//   } else { 
-//     clorbackgroung.classList.remove('background-color-alt');
-//     }
-
-// }
-// function cambiacara () {
-//   const selectValue = face.value;
-//   console.log(selectValue);
-//   faceInWeb.innerHTML = selectValue;
-// }
 
 
 
 
 
-
-
-
-
-
-
-// function handelButton () {
-//   const newSate = document.querySelector ()
-// }
