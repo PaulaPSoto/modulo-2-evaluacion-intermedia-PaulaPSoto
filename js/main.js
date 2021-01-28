47b489f338a8a29f6e39cc90479ffd0e5aff4e0b
PaulaPSoto <paezsotopaula@gmail.com>
2021-01-28 09:54:46 +0100

    Generate randomNumber

--- js/main.js
+++ js/main.js
@@ -1,35 +1,43 @@
 'use strict';
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+ }
+ 
+const randomNumber = getRandomNumber(100);
+console.log('El número aleatorio es:' + randomNumber);
 
-const face = document.querySelector ('.js-select');
-const buttonChange = document.querySelector ('.js-update');
-const faceInWeb = document.querySelector ('.js-face');
-const clorbackgroung = document.querySelector ('.js-container');
 
-function colorfondo (){
-  const randomNumber = Math.ceil(Math.random() * 100);
-  const tipo=(randomNumber%2)?"Impar":"Par";
 
-  if (tipo=== "Par") {
-    clorbackgroung.classList.add('background-color-alt');
+// const face = document.querySelector ('.js-select');
+// const buttonChange = document.querySelector ('.js-update');
+// const faceInWeb = document.querySelector ('.js-face');
+// const clorbackgroung = document.querySelector ('.js-container');
+
+// function colorfondo (){
+//   const randomNumber = Math.ceil(Math.random() * 100);
+//   const tipo=(randomNumber%2)?"Impar":"Par";
+
+//   if (tipo=== "Par") {
+//     clorbackgroung.classList.add('background-color-alt');
     
-  } else { 
-    clorbackgroung.classList.remove('background-color-alt');
-    }
+//   } else { 
+//     clorbackgroung.classList.remove('background-color-alt');
+//     }
 
-}
-function cambiacara () {
-  const selectValue = face.value;
-  console.log(selectValue);
-  faceInWeb.innerHTML = selectValue;
-}
+// }
+// function cambiacara () {
+//   const selectValue = face.value;
+//   console.log(selectValue);
+//   faceInWeb.innerHTML = selectValue;
+// }
 
-function handleButtonClick (event) {
- colorfondo();
- cambiacara();
-}
+// function handleButtonClick (event) {
+//  colorfondo();
+//  cambiacara();
+// }
 
 
-buttonChange.addEventListener ('click', handleButtonClick);
+// buttonChange.addEventListener ('click', handleButtonClick);
 
 
 
