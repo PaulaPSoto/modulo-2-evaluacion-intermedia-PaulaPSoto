3ceeb476b24affbcc6c45b8dc34336301bac0da9
PaulaPSoto <paezsotopaula@gmail.com>
2021-04-21 11:59:45 +0200

    add functionality with Js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
-'use strict';
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
- }
- 
-const randomNumber = getRandomNumber(100);
-console.log('El número aleatorio es:' + randomNumber);
+"use strict";
+const introNumber = document.querySelector(".js-test");
+const game = document.querySelector(".js-numbergame");
+const writeText = document.querySelector(".js-textgame");
 
-const introNumber = document.querySelector('.js-test');
-const game = document.querySelector('.js-numbrergame');
-const writeText = document.querySelector('.js-textgame');
 const total = 0;
 
+// Prevent the form from being sent
 
-function handleButtonClick (event) {
-  const selectValue = game.value;
-  console.log('El número introducido es: ' + selectValue);
-
-  if (game===randomNumber) {
-
-    writeText.innerHTML='¡Has Ganado!';
-   } else { 
-    writeText.innerHTML='¡Sigue jugando!'; 
-    
-   }
+function handleFormSubmit(event) {
+  event.preventDefault();
 }
- introNumber.addEventListener('click', handleButtonClick);
+introNumber.addEventListener("click", handleFormSubmit);
 
+// Random number function
 
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+//RandomNumber max=100
+const randomNumber = getRandomNumber(100);
+console.log("El número aleatorio es:" + randomNumber);
 
+function gameResult(newText) {
+  writeText.innerHTML = newText;
+}
 
+function handleButtonClick(event) {
+  const selectValue = game.value;
+  console.log("El número introducido es: " + selectValue);
 
+  if (game === randomNumber) {
+    gameResult("¡Has Ganado!");
+  } else if (game !== randomNumber) {
+    gameResult("¡Sigue intentándolo!");
+  }
+}
 
-
-
-
-
-
+introNumber.addEventListener("click", handleButtonClick);
